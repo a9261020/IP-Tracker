ff2836c8e9b86641263372ceea183246b3718f07
James <james.lee@biji.co>
2020-11-18 17:47:42 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "airbnb",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: [
+    "react",
+  ],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": "off",
+    "react/jsx-indent": [2, 4],
+    "react/jsx-indent-props": [2, 4],
+    quotes: [2, "double"],
+    indent: [2, 4],
+  },
+};
