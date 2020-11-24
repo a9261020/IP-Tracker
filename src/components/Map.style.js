ea518e1c1df6da9217056c119993dc66cdebfad7
James <james.lee@biji.co>
2020-11-24 11:40:04 +0800

    feat: leaflet done

--- src/components/Map.style.js
+++ src/components/Map.style.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import device from "../device";
 
 const Map = styled.div`
-    width: 100%;
+    width: 100vw;
     height: 70vh;
     background-color: #999;
 
